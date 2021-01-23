4724dd0e9ad8457c0100e65ae73e81751a5eac88
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-01-23 13:36:19 -0300

    Typewriter effect on homepage

--- mono/accounts/models.py
+++ mono/accounts/models.py
@@ -10,8 +10,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=256, blank=True, null=True)
     gender = models.CharField(
-        max_length=1, choices=(('', 'Gender'), ('m', 'Male'), ('f', 'Female'), ('n', 'I prefer not to say')),
-        blank=False, null=False)
+        max_length=1, 
+        choices=(
+            ('', 'Gender'), 
+            ('m', 'Male'), 
+            ('f', 'Female'), 
+            ('n', 'I prefer not to say')),
+        blank=False, 
+        null=False)
 
     avatar = models.ImageField(null=True)
 
