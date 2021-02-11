bc07d588b4ff9aba37b6d44b8a8a1b04da62b5b9
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-02-10 22:05:31 -0300

    Initial models and data for finance app

--- mono/finance/admin.py
+++ mono/finance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Transaction)
+admin.site.register(models.Group)
+admin.site.register(models.Icon)
+admin.site.register(models.Category)
--- mono/finance/models.py
+++ mono/finance/models.py
@@ -1,28 +1,49 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.enums import Choices
 
 User = get_user_model()
 
-# Create your models here.
-class TransactionModel(models.Model):
+# Create your models here.  
+class Transaction(models.Model):
+
+    INCOME = 'INC'
+    EXPENSE = 'EXP'
+    TRANSACTION_TYPES = [
+        (INCOME, 'Income'),
+        (EXPENSE, 'Expense'),
+    ]
+
     description = models.CharField(max_length=50, null=False, blank=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     value = models.FloatField()
+    type = models.CharField(max_length=3, choices=TRANSACTION_TYPES, default=EXPENSE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    active = models.BooleanField(default=True)
+    def __str__(self) -> str:
+        return self.description
+
+class Icon(models.Model):
+    markup = models.CharField(max_length=50, unique=True)
+    def __str__(self) -> str:
+        return self.markup
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
     active = models.BooleanField(default=True)
     def __str__(self) -> str:
         return self.name
     class Meta:
-        abstract = True
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
-class Expense(TransactionModel):
-    deadline = models.DateTimeField()
-    assigned_to = models.ManyToManyField(User, related_name="assigned_projects")
-
-class Income(TransactionModel):
-    deadline = models.DateTimeField()
-    assigned_to = models.ManyToManyField(User, related_name="assigned_projects")
-
-class Family(models.Model):
-    deadline = models.DateTimeField()
-    name = models.CharField(verbose_name="Nome", max_length=50)
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    member = models.ManyToManyField(User)
+    def __str__(self) -> str:
+        return self.name
