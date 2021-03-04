fbddb0caa8fc4b53fb520be6450fbe13f4704e4d
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-03-04 08:45:14 -0300

    Prod configurations commented

--- mono/_mono/settings.py
+++ mono/_mono/settings.py
@@ -146,12 +146,12 @@ if os.environ['APP_ENV'] == 'DEV':
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'    
-    EMAIL_HOST
-    EMAIL_PORT
-    EMAIL_HOST_USER
-    EMAIL_HOST_PASSWORD
-    EMAIL_USE_TLS
-    EMAIL_USE_SSL
-    EMAIL_TIMEOUT
-    EMAIL_SSL_KEYFILE
-    EMAIL_SSL_CERTFILE
+    # EMAIL_HOST
+    # EMAIL_PORT
+    # EMAIL_HOST_USER
+    # EMAIL_HOST_PASSWORD
+    # EMAIL_USE_TLS
+    # EMAIL_USE_SSL
+    # EMAIL_TIMEOUT
+    # EMAIL_SSL_KEYFILE
+    # EMAIL_SSL_CERTFILE
