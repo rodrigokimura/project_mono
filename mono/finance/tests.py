4f76133230d72c74ed8729cb915b7db7ac4d0fb9
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-03-09 22:33:49 -0300

    Test improvements

--- mono/finance/tests.py
+++ mono/finance/tests.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.utils import timezone
 from django.contrib.auth import get_user_model
-from .models import Transaction, Account, Category
+from .models import Transaction, Account, Category, Group
 import datetime
+from django.contrib.auth.models import AnonymousUser, User
+
+
 
 User = get_user_model()
 # Create your tests here.
@@ -55,6 +58,51 @@ class AccountModelTests(TestCase):
 
         account.adjust_balance(-100, self.user)
         self.assertLess(account.current_balance, 0)
+
+class AccountModelTests(TestCase):
+
+    fixtures = ["icon.json"]
+
+    def setUp(self):
+        self.user_1 = User.objects.create(username="User_1")
+        self.user_2 = User.objects.create(username="User_2")
+        self.group = Group.objects.create(
+            name="Group", 
+            owned_by=self.user_1,
+            created_by=self.user_1
+        )
+        self.assertIsNotNone(self.group)
+        self.group.members.add(self.user_1)
+        self.assertEquals(self.group.members.count(), 1)
+        self.group.members.add(self.user_2)
+        self.assertGreater(self.group.members.count(), 1)
+        self.account = Account.objects.filter(owned_by=self.user_1).first()
+        self.factory = RequestFactory()
+
+
+    def test_group_creation(self):
+        self.assertTrue(self.group.owned_by==self.user_1)
+        self.assertFalse(self.group.owned_by==self.user_2)
+
+    def test_patch_with_logged_in_user(self):
+        transaction = Transaction.objects.create(
+            description="Test", 
+            ammount=100, 
+            account=self.account,
+            created_by=self.user_1
+        )
+        request = self.factory.get(f'/fn/transaction/{transaction.pk}/delete/')
+        request.user = self.user_1
+
+        # # Test my_view() as if it were deployed at /customer/details
+        # response = my_view(request)
+        # # Use this syntax for class-based views.
+        # response = MyView.as_view()(request)
+        # self.assertEqual(response.status_code, 200)
+
+
+
+
 
 
 # User default accounts creation
