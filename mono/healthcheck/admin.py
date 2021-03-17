9dd8baddffffe3536cfee3c68f10bfd6e8379143
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-03-17 17:57:46 -0300

    Deployment command

--- mono/healthcheck/admin.py
+++ mono/healthcheck/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.PullRequest)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = [
+        "number",
+        "author",
+        "commits",
+        "additions",
+        "deletions",
+        "changed_files",
+        "merged_at",
+        "received_at",
+        "pulled_at",
+        "deployed_at",        
+    ]
+    list_filter = [
+        "author",
+    ]
--- mono/healthcheck/models.py
+++ mono/healthcheck/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from pathlib import Path
+from django.conf import settings
+import git
+from django.utils import timezone
+from django.core.mail import mail_admins
+from django.template.loader import get_template
 
 # Create your models here.
 class PullRequest(models.Model):
@@ -27,3 +33,72 @@ class PullRequest(models.Model):
     @property
     def deployed(self):
         return self.deployed_at is not None
+
+    def pull(self, **kwargs):
+        path = Path(settings.BASE_DIR).resolve().parent
+        repo = git.Repo(path)
+        origin = repo.remotes.origin
+        fetchInfoList = origin.pull()
+        print("Successfully pulled from remote.")
+        self.pulled_at = timezone.now()
+        self.save()
+
+        d = {
+            'title': 'Merged PR',
+            'warning_message': f'Pull Request #{self.number}',
+            'first_line': 'Detected merged PR',
+            'main_text_lines': [
+                f'Merged by: {self.author}',
+                f'Merged at: {self.merged_at}',
+                f'Commits: {self.commits}',
+                f'Additions: {self.additions}',
+                f'Deletions: {self.deletions}',
+                f'Changed files: {self.changed_files}',
+            ],
+            'button_link': kwargs['link'],
+            'button_text': f'Go to Pull Request #{self.number}',
+            'after_button': '',
+            'unsubscribe_link': None,
+        }
+
+        mail_admins(
+            subject=f'Delivery Notification - {self}', 
+            message=get_template('email/alert.txt').render(d),
+            html_message=get_template('email/alert.html').render(d)
+        )
+
+    def deploy(self):
+        # TODO: #102 Check for migrations to apply before reloading
+        # TODO: #103 Check for new static files to apply before reloading
+        try:
+            if settings.APP_ENV == 'PRD':
+                wsgi_file = '/var/www/www_monoproject_info_wsgi.py'
+                Path(wsgi_file).touch()
+                print(f"{wsgi_file} has been touched.")
+                
+            print(f"Successfully deployed {self}.")
+            self.deployed_at = timezone.now()
+            self.save()
+
+            d = {
+                'title': 'Merged PR',
+                'warning_message': f'Deployment Notification - {self}',
+                'first_line': f'{self} has been deployed.',
+                'main_text_lines': [
+                    f'Deployed at: {self.deployed_at}',
+                ],
+                'button_link': settings.ALLOWED_HOSTS[0],
+                'button_text': 'Go to app',
+                'after_button': '',
+                'unsubscribe_link': None,
+            }
+
+            print("Notifying admins about the deployment.")
+            mail_admins(
+                subject=f'Deploy Notification', 
+                message=get_template('email/alert.txt').render(d),
+                html_message=get_template('email/alert.html').render(d)
+            )
+            return True
+        except Exception as e:
+            return e
