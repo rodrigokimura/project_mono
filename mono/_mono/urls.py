231fd7625da46ceb44d15ad64e0eaab3e17b2d9a
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-03-18 08:53:33 -0300

    Add Django Admin Docs Fixes #107

--- mono/_mono/urls.py
+++ mono/_mono/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 # import debug_toolbar
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('accounts/', include('accounts.urls')),
