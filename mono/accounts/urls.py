2f3cab1a7eab4bb493e09b1b416781e885884358
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-05 08:13:13 -0300

    Email verification - WIP

--- mono/accounts/urls.py
+++ mono/accounts/urls.py
@@ -1,16 +1,7 @@
-from django.conf.urls import url
-from . import views
+from django.urls import path
 
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r"login/$", views.Login.as_view(), name='login'),
-    url(r"logout/$", views.Logout.as_view(), name="logout"),
-    url(r"password_change/$", views.PasswordChange.as_view(), name="password_change"),
-    url(r"password_change/done/$", views.PasswordChangeDone.as_view(), name="password_change_done"),
-    url(r"password_reset/$", views.PasswordReset.as_view(), name="password_reset"),
-    url(r"password_reset/done/$", views.PasswordResetDone.as_view(), name="password_reset_done"),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.PasswordResetConfirm.as_view(), name="password_reset_confirm"),
-    url(r"reset/done/$", views.PasswordResetComplete.as_view(), name="password_reset_complete"),
-    url(r"signup/$", views.sign_up, name="signup"),
+    # path("", views.HomePageView.as_view(), name='index'),
 ]
--- mono/accounts/views.py
+++ mono/accounts/views.py
@@ -1,81 +1,31 @@
-from django.contrib.auth import views
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.shortcuts import render
-
-from . import forms
-
-
-class SignUp(CreateView):
-    form_class = forms.UserCreateForm
-    success_url = reverse_lazy("accounts:login")
-    template_name = "accounts/signup.html"
+from mono.accounts.models import User, UserProfile
+from django.conf import settings
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+import jwt
 
 
-def sign_up(request):
-    template = "accounts/signup.html"
-    if request.method == "POST":
-        uform = forms.UserCreateForm(data=request.POST)
-        pform = forms.UserProfileForm(data=request.POST, files=request.FILES)
-        if uform.is_valid() and pform.is_valid():
-            user = uform.save()
-            profile = pform.save(commit=False)
-            profile.user = user
-            profile.save()
-        else:
-            for field in uform.errors:
-                uform[field].field.widget.attrs.update({'class': 'field error'})
-            for field in pform.errors:
-                pform[field].field.widget.attrs.update({'class': 'field error'})
-    elif request.method == "GET":
-        uform = forms.UserCreateForm()
-        pform = forms.UserProfileForm()
+class AccountVerificationView(LoginRequiredMixin, TemplateView):
+    template_name = "finance/invite_acceptance.html"
 
-    return render(request, template, {
-        "uform": uform,
-        "pform": pform
-    })
+    def get(self, request):
+        token = request.GET.get('t', None)
 
+        if token is None:
+            return HttpResponse("error")
 
-class Login(views.LoginView):
-    template_name = "accounts/login.html"
-    success_url = reverse_lazy("homepage:home")
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=["HS256"]
+        )
 
-
-class Logout(views.LogoutView):
-    next_page = "/"
-
-
-class PasswordChange(views.PasswordChangeView):
-    template_name = "accounts/password_change.html"
-    success_url = reverse_lazy("accounts:password_change_done")
-
-
-class PasswordChangeDone(views.PasswordChangeDoneView):
-    template_name = "accounts/password_change_done.html"
-    extra_context = {"mensagem": "Senha alterada com sucesso!"}
-
-
-class PasswordReset(views.PasswordResetView):
-    template_name = "accounts/password_reset.html"
-    email_template_name = "accounts/password_reset_email.html"
-    html_email_template_name = "accounts/password_reset_email.html"
-    subject_template_name = "accounts/password_reset_subject.txt"
-    success_url = reverse_lazy("accounts:password_reset_done")
-    form_class = forms.CustomPasswordResetForm
-
-
-class PasswordResetDone(views.PasswordResetDoneView):
-    template_name = "accounts/password_reset_done.html"
-    extra_context = {"mensagem": "O link para redefinição de senha foi enviado ao email cadastrado!"}
-
-
-class PasswordResetConfirm(views.PasswordResetConfirmView):
-    template_name = "accounts/password_reset_confirm.html"
-    post_reset_login = True
-    success_url = reverse_lazy("accounts:password_reset_complete")
-
-
-class PasswordResetComplete(views.PasswordResetCompleteView):
-    template_name = "accounts/password_reset_complete.html"
-    extra_context = {"mensagem": "A senha foi redefinida com sucesso!"}
+        user = get_object_or_404(User, pk=payload['user_id'])
+        if request.user == user:
+            profile, created = UserProfile.objects.get_or_create(user=user)
+            profile.verify()
+        return self.render_to_response({
+            "accepted": True,
+        })
