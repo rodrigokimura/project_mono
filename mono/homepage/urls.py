a7852ddd072f38c29ed053ef44d17bc903bcbe7e
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-14 08:39:00 -0300

    Markdown and homepage redirect

--- mono/homepage/urls.py
+++ mono/homepage/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # path('', views.index, name='home'),
+    path('', RedirectView.as_view(pattern_name='finance:index'), name='home'),
 ]
