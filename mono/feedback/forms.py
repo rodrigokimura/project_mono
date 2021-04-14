7d1a21126f4a965ac394f9fa75285752b187d19c
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-14 14:45:05 -0300

    Feedback collector Fixes #229

---
+++ mono/feedback/forms.py
@@ -0,0 +1,78 @@
+from django import forms
+from .models import Feedback
+from django.forms.widgets import Widget
+from django.template.loader import get_template
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
+
+
+class ButtonsWidget(Widget):
+    template_name = 'widgets/ui_icon_buttons.html'
+
+    def __init__(self, attrs=None, *args, **kwargs):
+        self.choices = kwargs.pop('choices')
+        super().__init__(attrs)
+
+    def get_context(self, name, value, attrs=None):
+        return {
+            'widget': {
+                'name': name,
+                'value': value,
+                'choices': self.choices,
+            },
+        }
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = self.get_context(name, value, attrs)
+        template = get_template(self.template_name).render(context)
+        return mark_safe(template)
+
+
+class ToggleWidget(Widget):
+    template_name = 'widgets/ui_toggle.html'
+
+    def get_context(self, name, value, attrs=None):
+        return {
+            'widget': {
+                'name': name,
+                'value': value,
+                'label': name.title(),
+            },
+        }
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = self.get_context(name, value, attrs)
+        template = get_template(self.template_name).render(context)
+        return mark_safe(template)
+
+
+class FeedbackForm(forms.ModelForm):
+    error_css_class = 'error'
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super().__init__(*args, **kwargs)
+        self.fields['message'].widget.attrs.update(
+            {
+                'placeholder': _('Please, tell us what is on your mind.')
+            }
+        )
+
+    def save(self, *args, **kwargs):
+        feedback = self.instance
+        feedback.user = self.request.user
+        feedback.save()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        model = Feedback
+        fields = ['feeling', 'message', 'public']
+        widgets = {
+            'feeling': ButtonsWidget(choices=Feedback.FEELING_CHOICES),
+            'public': ToggleWidget,
+        }
+        error_messages = {
+            'feeling': {
+                'invalid_choice': _("Please, select a valid option.")
+            }
+        }
