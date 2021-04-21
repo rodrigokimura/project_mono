5542063225eba448af6977c6102c5bb1cf3115b0
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-21 11:11:56 -0300

    Signal to round transactions

--- mono/healthcheck/apps.py
+++ mono/healthcheck/apps.py
@@ -15,4 +15,4 @@ class HealthcheckConfig(AppConfig):
         post_save.connect(group_initial_setup, sender=Group, dispatch_uid="group_initial_setup")
         post_save.connect(installments_creation, sender=Installment, dispatch_uid="installments_creation")
         post_save.connect(email_verification, sender=User, dispatch_uid="email_verification")
-        # post_save.connect(round_transaction, sender=Transaction, dispatch_uid="round_transaction")
+        post_save.connect(round_transaction, sender=Transaction, dispatch_uid="round_transaction")
