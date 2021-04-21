109f1e5b6442275c4d92ce5eb20940ebc2084249
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-21 11:09:13 -0300

    Hotfix rounding

--- mono/healthcheck/apps.py
+++ mono/healthcheck/apps.py
@@ -7,11 +7,12 @@ class HealthcheckConfig(AppConfig):
     name = 'healthcheck'
 
     def ready(self):
-        from finance.signals import initial_setup, group_initial_setup, installments_creation
+        from finance.signals import initial_setup, group_initial_setup, installments_creation, round_transaction
         from accounts.signals import email_verification
-        from finance.models import Group, Installment
+        from finance.models import Group, Installment, Transaction
         User = get_user_model()
         post_save.connect(initial_setup, sender=User, dispatch_uid="user_initial_setup")
         post_save.connect(group_initial_setup, sender=Group, dispatch_uid="group_initial_setup")
         post_save.connect(installments_creation, sender=Installment, dispatch_uid="installments_creation")
         post_save.connect(email_verification, sender=User, dispatch_uid="email_verification")
+        # post_save.connect(round_transaction, sender=Transaction, dispatch_uid="round_transaction")
