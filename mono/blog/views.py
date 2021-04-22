254c7469b01e17dd952794b3f2ee3e28fb11f2e2
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2021-04-22 20:42:55 -0300

    Blog app improvements

--- mono/blog/views.py
+++ mono/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# Create your views here.
+
+class PostCreateView(UserPassesTestMixin, SuccessMessageMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:posts')
+    success_message = "%(title)s was created successfully"
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class PostUpdateView(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:posts')
+    success_message = "%(title)s was updated successfully"
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class PostDeleteView(UserPassesTestMixin, SuccessMessageMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:posts')
+    success_message = "Post was deleted successfully"
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
