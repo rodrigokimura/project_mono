00c748f25adb27803ba19420fc50c1da80d57fd2
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-06-05 11:04:32 -0300

    Boards CRUD

--- mono/project_manager/forms.py
+++ mono/project_manager/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Project
+from .models import Project, Board
 from .widgets import CalendarWidget, ToggleWidget
 
 
@@ -29,5 +29,31 @@ class ProjectForm(forms.ModelForm):
         ]
         widgets = {
             'deadline': CalendarWidget,
+            'active': ToggleWidget,
+        }
+
+
+class BoardForm(forms.ModelForm):
+    error_css_class = 'error'
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_to'].widget.attrs.update({'class': 'ui dropdown'})
+
+    def save(self, *args, **kwargs):
+        board = self.instance
+        if board.pk is None:
+            board.created_by = self.request.user
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        model = Board
+        fields = [
+            'name',
+            'assigned_to',
+            'active',
+        ]
+        widgets = {
             'active': ToggleWidget,
         }
