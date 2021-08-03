a544ab1ea73719f6775ce54d229eb1fdd39dc1ac
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-08-03 14:01:19 -0300

    Make and apply migrations

--- mono/project_manager/models.py
+++ mono/project_manager/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
