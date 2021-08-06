0492fbaa154bf672de63fd155319848f95a97cf3
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-08-05 22:08:22 -0300

    Implement colors on cards and buckets

--- mono/project_manager/apps.py
+++ mono/project_manager/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save, pre_save
 
 
 class ProjectManagerConfig(AppConfig):
     name = 'project_manager'
 
     def ready(self):
-        from project_manager.models import TimeEntry, Board
-        from project_manager.signals import calculate_duration, create_default_buckets
-        pre_save.connect(calculate_duration, sender=TimeEntry, dispatch_uid="calculate_duration")
-        post_save.connect(create_default_buckets, sender=Board, dispatch_uid="create_default_buckets")
+        import project_manager.signals
+        print(f'Connecting signals from: {project_manager.signals.__name__}')
