94e15d6eae5758f91c9a76b35e561734a0a1ae5b
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-08-17 22:00:57 -0300

    Change database config

--- mono/_mono/settings.py
+++ mono/_mono/settings.py
@@ -145,7 +145,8 @@ else:  # pragma: no cover
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASS'),
             'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
             }
         }
     }
