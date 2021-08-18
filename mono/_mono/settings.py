97ae41e825010ddac201150931d08a3331126c7d
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-08-17 22:14:22 -0300

    Change database config

--- mono/_mono/settings.py
+++ mono/_mono/settings.py
@@ -147,6 +147,7 @@ else:  # pragma: no cover
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
+                'use_unicode': True,
             }
         }
     }
