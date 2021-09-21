7f4d5c6c8e5973f4bff6568110c5efbfc6f80e07
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-09-21 11:02:34 -0300

    Change dashboard page

--- mono/static/pixeldashboard.js
+++ mono/static/pixeldashboard.js
@@ -1,3 +1,16 @@
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    return hours + ':' + minutes + ':' + seconds;
+}
+
+
 class Dashboard {
     constructor(id) {
         this.id = id;
@@ -52,7 +65,7 @@ class Dashboard {
                     chart: {
                         type: 'line',
                         fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;",
-                        height: '400px',
+                        height: '370px',
                     },
                     plotOptions: {
                         bar: {
@@ -107,23 +120,26 @@ class Dashboard {
             url: `/pixel/dashboard/${this.id}/by-doc-loc/?${params}`,
             headers: { 'X-CSRFToken': csrftoken },
             on: 'now',
-            stateContext: '.card-statistic',
+            stateContext: '#by-doc-loc .segments',
             onSuccess: r => {
                 $('#by-doc-loc row').remove();
                 r.data.forEach(d => {
-                    $('#by-doc-loc .segments.list').append(`
-                        <div class="row ui segment">
-                            <div class="ui grid">
-                                <div class="seven wide column doc-loc">${d.document_location}</div>
-                                <div class="three wide column views">${d.views}</div>
-                                <div class="three wide column visitors">${d.visitors}</div>
-                                <div class="three wide column duration">${d.duration}</div>
-                            </div>
+                    $('#by-doc-loc .list').append(`
+                        <div class="row">
+                            <div class="seven wide column doc-loc">${d.document_location}</div>
+                            <div class="three wide column right aligned views">${d.views}</div>
+                            <div class="three wide column right aligned visitors">${d.visitors}</div>
+                            <div class="three wide column right aligned duration" data-duration="${d.duration}">${d.duration.toHHMMSS()}</div>
                         </div>
                     `);
                 });
                 var options = {
-                    valueNames: ['doc-loc', 'views', 'visitors', 'duration']
+                    valueNames: [
+                        'doc-loc',
+                        'views',
+                        'visitors',
+                        { name: 'duration', attr: 'data-duration' },
+                    ]
                 };
                 var byDocLocList = new List('by-doc-loc', options);
             }
