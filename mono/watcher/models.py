da4877e67661381298af35667695654f57bdd1a9
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-10-04 18:14:02 -0300

    Add watcher app

--- mono/watcher/models.py
+++ mono/watcher/models.py
@@ -1,22 +1,56 @@
-import uuid
-from datetime import timedelta
-
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models.aggregates import Avg
-from django.utils import timezone
 
 
-class Error(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    host = models.CharField(
-        max_length=1024,
-        null=True,
-        help_text="Host of the URL. Port or userinfo should be ommited. https://en.wikipedia.org/wiki/URL"
-    )
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+class Issue(models.Model):
+    hash = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
-    deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+    # deleted_at = models.DateTimeField(null=True, blank=True, default=None)
 
     def __str__(self):
-        return str(self.id)
+        return self.name
+
+    @property
+    def users(self):
+        return self.event_set.values('user').distinct()
+
+
+class Event(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.issue)
+
+
+class Traceback(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    order = models.PositiveSmallIntegerField()
+    file_name = models.CharField(max_length=1000)
+    function_name = models.CharField(max_length=1000)
+    line_number = models.PositiveIntegerField()
+    code_text = models.TextField(max_length=2000)
+    variables = models.JSONField()
+
+    def __str__(self):
+        return str(self.issue)
+
+    @property
+    def short_file_name(self):
+        fn: str = self.file_name
+        if 'site-packages/' in fn:
+            return fn.split('site-packages/')[-1]
+        if 'project_mono/mono/' in fn:
+            return fn.split('project_mono/mono/')[-1]
+
+    # @property
+    # def code(self):
+    #     l: int = self.line_number
+    #     for line in code_text
+
+    @property
+    def vars(self):
+        return self.variables.items()
