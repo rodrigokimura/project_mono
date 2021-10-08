29b099e5a17030f41079f53081e56b693d000b50
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-10-07 21:21:52 -0300

    Change order in progress bar

--- mono/project_manager/models.py
+++ mono/project_manager/models.py
@@ -67,9 +67,9 @@ class Project(BaseModel):
             not_started_perc = 100 - completed_perc - in_progress_perc
 
         return {
-            'completed': [completed, completed_perc],
-            'in_progress': [in_progress, in_progress_perc],
             'not_started': [not_started, not_started_perc],
+            'in_progress': [in_progress, in_progress_perc],
+            'completed': [completed, completed_perc],
         }
 
 
@@ -127,9 +127,9 @@ class Board(BaseModel):
             not_started_perc = 100 - completed_perc - in_progress_perc
 
         return {
-            'completed': [completed, completed_perc],
-            'in_progress': [in_progress, in_progress_perc],
             'not_started': [not_started, not_started_perc],
+            'in_progress': [in_progress, in_progress_perc],
+            'completed': [completed, completed_perc],
         }
 
 
