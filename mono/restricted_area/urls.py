1cc3ab59297bd42eede029a66cbdec12e17a589d
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-10-17 15:27:21 -0300

    Format code

--- mono/restricted_area/urls.py
+++ mono/restricted_area/urls.py
@@ -6,18 +6,18 @@ app_name = 'restricted-area'
 
 urlpatterns = [
     path(
-        '', 
-        views.IndexView.as_view(), 
+        '',
+        views.IndexView.as_view(),
         name='index'
     ),
     path(
-        'force-error-500/', 
-        views.ForceError500View.as_view(), 
+        'force-error-500/',
+        views.ForceError500View.as_view(),
         name='force-error-500'
     ),
     path(
-        'view-error-500/', 
-        views.ViewError500View.as_view(), 
+        'view-error-500/',
+        views.ViewError500View.as_view(),
         name='view-error-500'
     ),
 ]
