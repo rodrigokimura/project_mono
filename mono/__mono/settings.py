0d238ccfb817fc4daca8f51847a52ca3f4fdeafc
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-12-01 20:38:02 -0300

    fix(finance): :ambulance: fix wrong code

--- mono/__mono/settings.py
+++ mono/__mono/settings.py
@@ -252,7 +252,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
