57dea986e8e33464f4e60359085a69d1b17c990d
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-12-02 10:12:08 -0300

    build(mono): update build script to tail server log file

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,4 @@ python mono/manage.py collectstatic --noinput
 python mono/manage.py makemigrations
 python mono/manage.py migrate
 touch /var/www/www_monoproject_info_wsgi.py
+tail /var/log/www.monoproject.info.server.log
