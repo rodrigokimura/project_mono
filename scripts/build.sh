bda9159a1ea644f4830af74f7f31d9f7c07bb51e
rodrigokimura <rodrigoeitikimura@gmail.com>
2021-12-02 10:20:33 -0300

    chore(mono): remove collected static files folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ python mono/manage.py collectstatic --noinput
 python mono/manage.py makemigrations
 python mono/manage.py migrate
 touch /var/www/www_monoproject_info_wsgi.py
-tail /var/log/www.monoproject.info.server.log
+tail /var/log/www.monoproject.info.server.log -n 100
