b4f457f870c70bb251d0abc26ca67ab4367e333b
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-01-20 14:00:32 -0300

    refactor(accounts): move models, views and routes related to stripe

--- mono/finance/views.py
+++ mono/finance/views.py
@@ -2,8 +2,6 @@ import time
 from datetime import datetime
 
 import jwt
-import pytz
-import stripe
 from __mono.mixins import PassRequestToFormViewMixin
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
@@ -11,19 +9,15 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
-from django.core.exceptions import SuspiciousOperation
 from django.db.models import F, FloatField, Q, Sum, Value as V
 from django.db.models.functions import Coalesce, TruncDay
 from django.http import HttpResponse, JsonResponse
 from django.http.response import Http404
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.urls.base import reverse
 from django.utils import timezone
-from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
 from django.views.generic.dates import MonthArchiveView
 from django.views.generic.detail import DetailView
@@ -34,7 +28,6 @@ from django.views.generic.list import ListView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from social_django.models import UserSocialAuth
 
 from .forms import (
     AccountForm, BudgetConfigurationForm, BudgetForm, CategoryForm, FakerForm,
@@ -43,8 +36,8 @@ from .forms import (
 )
 from .models import (
     Account, Budget, BudgetConfiguration, Category, Chart, Configuration, Goal,
-    Group, Icon, Installment, Invite, Plan, RecurrentTransaction, Subscription,
-    Transaction, Transference,
+    Group, Icon, Installment, Invite, RecurrentTransaction, Transaction,
+    Transference,
 )
 from .permissions import IsCreator
 from .serializers import ChartMoveSerializer, ChartSerializer
@@ -954,303 +947,6 @@ class FakerView(UserPassesTestMixin, FormView):
         messages.add_message(self.request, message['level'], message['message'])
 
         return super().form_valid(form)
-
-
-class PlansView(UserPassesTestMixin, TemplateView):
-    template_name = "finance/plans.html"
-
-    def test_func(self):
-        return self.request.user.is_superuser
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-
-        # Get all Stripe products
-        products = stripe.Product.list(limit=100, active=True).data
-        if len(products) == 100:
-            next_page = True
-            max_loops = 10
-            loop = 0
-            last_product = products[-1]
-            while next_page and loop < max_loops:
-                loop += 1
-                new_products = stripe.Product.list(limit=100, starting_after=last_product).data
-                if len(new_products) == 100:
-                    products.extend(new_products)
-                    last_product = new_products[-1]
-                else:
-                    next_page = False
-
-        # Filter products by metada {app:finance}
-        products = filter(lambda product: hasattr(product.metadata, 'app'), products)
-        products = [product for product in products if product.metadata.app == 'finance']
-
-        # Sort products according to business rules
-        pass
-
-        context['plans'] = Plan.objects.filter(product_id__in=[product.id for product in products])
-        context['free_plan'] = Plan.objects.filter(
-            product_id__in=[product.id for product in products],
-            type=Plan.FREE
-        ).first()
-        if self.request.user.is_authenticated:
-            if Subscription.objects.filter(user=self.request.user).exists():
-                user_plan = Subscription.objects.get(user=self.request.user).plan
-            else:
-                user_plan = Plan.objects.get(type=Plan.FREE)
-        else:
-            user_plan = None
-        context['user_plan'] = user_plan
-
-        return context
-
-
-class CheckoutView(UserPassesTestMixin, TemplateView):
-    template_name = "finance/checkout.html"
-
-    def test_func(self):
-        return self.request.user.is_superuser
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['stripe_pk'] = settings.STRIPE_PUBLIC_KEY
-        # Check query param 'plan'
-        plan_id = self.request.GET.get("plan", None)
-        if plan_id not in ["", None]:
-            plan = get_object_or_404(Plan, pk=plan_id)
-            context['plan'] = plan
-        else:
-            raise SuspiciousOperation("Invalid request. This route need a query parameter 'plan'.")
-
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            product = stripe.Product.retrieve(plan.product_id)
-            context['product'] = product
-        except Exception as e:
-            print(e)
-            raise SuspiciousOperation(e)
-
-        # Get currency from language
-        if self.request.LANGUAGE_CODE == 'pt-br':
-            currency = 'brl'
-        else:
-            currency = 'usd'
-
-        # Get Plans for the plan
-        plans = stripe.Plan.list(product=product.id, active=True, currency=currency).data
-
-        # Get monthly plan price
-        plans_for_context = []
-        try:
-            monthly_price = list(filter(lambda price: price.interval == 'month', plans))[0]
-            plans_for_context.append(monthly_price)
-        except Exception as e:
-            print(e)
-
-        # Get yearly plan price
-        try:
-            yearly_price = list(filter(lambda price: price.interval == 'year', plans))[0]
-            plans_for_context.append(yearly_price)
-        except Exception as e:
-            print(e)
-
-        context['plans'] = plans_for_context
-
-        return context
-
-    def get(self, request):
-        plan_id = self.request.GET.get("plan", None)
-        plan = get_object_or_404(Plan, pk=plan_id)
-        if plan.type == Plan.FREE:
-            if Subscription.objects.filter(user=request.user).exists():
-                subscription = Subscription.objects.get(user=request.user)
-
-                (success, message) = subscription.cancel_at_period_end()
-                if success:
-                    messages.success(request, message)
-                else:
-                    messages.error(request, message)
-                return redirect(to=reverse('finance:plans'))
-            else:
-                messages.error(request, "You are already subscribed to the Free Plan.")
-                return redirect(to=reverse('finance:plans'))
-        else:
-            return self.render_to_response(self.get_context_data())
-
-    def post(self, request):
-        payment_method_id = request.POST.get("payment_method_id")
-        price_id = request.POST.get("price_id")
-
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-
-        email = self.request.user.email
-
-        # Check if current user is a stripe Customer
-        customer_list = stripe.Customer.list(email=email).data
-
-        if len(customer_list) == 0:
-            # user is not a stripe Customer
-            # creating new customer
-            customer = stripe.Customer.create(email=email)
-        elif len(customer_list) == 1:
-            # user is a stripe Customer
-            customer = customer_list[0]
-        else:
-            # multiple users returned
-            customer = customer_list[-1]
-
-        # Get all Stripe payment methods for the user
-        payment_methods = stripe.PaymentMethod.list(customer=customer.id, type="card", limit=100).data
-        if len(payment_methods) == 100:
-            next_page = True
-            max_loops = 10
-            loop = 0
-            last_payment_method = payment_methods[-1]
-            while next_page and loop < max_loops:
-                loop += 1
-                new_payment_methods = stripe.PaymentMethod.list(
-                    customer=customer.id,
-                    type="card",
-                    limit=100,
-                    starting_after=last_payment_method
-                ).data
-                if len(new_payment_methods) == 100:
-                    payment_methods.extend(new_payment_methods)
-                    last_payment_method = new_payment_methods[-1]
-                else:
-                    next_page = False
-
-        # Dettaching all payment methods
-        for payment_method in payment_methods:
-            stripe.PaymentMethod.detach(payment_method.id)
-
-        # Attach new payment method to customer
-        stripe.PaymentMethod.attach(
-            payment_method_id,
-            customer=customer.id,
-        )
-
-        # Set as default payment method
-        customer.modify(
-            customer.id,
-            invoice_settings={"default_payment_method": payment_method_id}
-        )
-
-        # Check if customer has a subscription
-        subscription_list = stripe.Subscription.list(customer=customer.id).data
-        if len(subscription_list) == 0:
-            # no subscriptions yet
-            # creating new subscription
-            subscription = stripe.Subscription.create(
-                customer=customer.id,
-                items=[{"price": price_id}]
-            )
-        elif len(subscription_list) == 1:
-            # already subscribed
-            subscription = subscription_list[0]
-            return JsonResponse(
-                {
-                    'success': False,
-                    'message': "You already have an active subscription.",
-                    'results': {
-                        'customer': customer.id,
-                        'subscription': subscription.id,
-                    }
-                }
-            )
-        else:
-            # multiple subscriptions returned
-            subscription = subscription_list[-1]
-
-        return JsonResponse(
-            {
-                'success': True,
-                'message': "You've successfully subscribed!",
-                'results': {
-                    'customer': customer.id,
-                    'subscription': subscription.id,
-                }
-            }
-        )
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class StripeWebhookView(View):
-
-    def post(self, request):
-        payload = request.body
-        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-        event = None
-
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-
-        try:
-            event = stripe.Webhook.construct_event(
-                payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-            )
-        except ValueError as e:
-            # Invalid payload
-            print(e)
-            return HttpResponse(status=400)
-        except stripe.error.SignatureVerificationError as e:
-            # Invalid signature
-            print(e)
-            return HttpResponse(status=400)
-        print(event['type'])
-        if event['type'] == 'customer.subscription.created':
-            user = User.objects.get(
-                email=stripe.Customer.retrieve(event.data.object.customer).email
-            )
-            plan = Plan.objects.get(
-                product_id=stripe.Price.retrieve(event.data.object['items'].data[0].price.id).product
-            )
-            subscription, created = Subscription.objects.update_or_create(
-                {
-                    'plan': plan,
-                    'event_id': event.id
-                },
-                user=user
-            )
-        elif event['type'] == 'customer.subscription.updated':
-
-            # Check for cancellation updates
-            subscription = event.data.object
-
-            # Convert to timezone aware timestamp, based on Stripe timezone configuration
-            if subscription.cancel_at:
-                cancellation_timestamp = timezone.make_aware(
-                    datetime.fromtimestamp(subscription.cancel_at),
-                    pytz.timezone(settings.STRIPE_TIMEZONE)
-                )
-            else:
-                cancellation_timestamp = None
-
-            # Update cancellation_timestamp and plan
-            user = User.objects.get(
-                email=stripe.Customer.retrieve(event.data.object.customer).email
-            )
-            plan = Plan.objects.get(
-                product_id=stripe.Price.retrieve(event.data.object['items'].data[0].price.id).product
-            )
-            subscription, created = Subscription.objects.update_or_create(
-                {
-                    'plan': plan,
-                    'cancel_at': cancellation_timestamp,
-                    'event_id': event.id,
-                },
-                user=user
-            )
-
-        elif event['type'] == 'customer.subscription.deleted':
-            user = User.objects.get(
-                email=stripe.Customer.retrieve(event.data.object.customer).email
-            )
-            if Subscription.objects.filter(user=user).exists():
-                Subscription.objects.get(user=user).delete()
-
-        return HttpResponse(status=200)
 
 
 class RestrictedAreaView(LoginRequiredMixin, TemplateView):
--- mono/pixel/tests.py
+++ mono/pixel/tests.py
@@ -2,7 +2,6 @@ from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.test.client import RequestFactory
-from django.views.generic.edit import FormView
 
 from .admin import SiteAdmin
 from .forms import SiteForm
