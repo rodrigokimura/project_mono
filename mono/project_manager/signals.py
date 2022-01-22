30103698982f9149c0940e32225f1a4f27fd4682
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2022-01-22 11:03:29 -0300

    refactor(restricted_area): apply pylint changes

--- mono/project_manager/signals.py
+++ mono/project_manager/signals.py
@@ -1,3 +1,4 @@
+"""Project manager's signals"""
 import logging
 import os
 
@@ -8,13 +9,19 @@ from .models import Board, Bucket, CardFile, Comment, Invite, TimeEntry
 
 
 @receiver(pre_save, sender=TimeEntry, dispatch_uid="calculate_duration")
-def calculate_duration(sender, instance, **kwargs):
+def calculate_duration(sender, instance, **kwargs):  # pylint: disable=W0613
+    """
+    Calculate duration of time entry
+    """
     if instance.stopped_at is not None:
         instance.duration = instance.stopped_at - instance.started_at
 
 
 @receiver(post_save, sender=Board, dispatch_uid="create_default_buckets")
-def create_default_buckets(sender, instance, created, **kwargs):
+def create_default_buckets(sender, instance, created, **kwargs):  # pylint: disable=W0613
+    """
+    Create default buckets on board creation
+    """
     if created:
         for name, desc, order, auto_status in Bucket.DEFAULT_BUCKETS:
             Bucket.objects.create(
@@ -28,20 +35,29 @@ def create_default_buckets(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=Invite, dispatch_uid="send_project_invite")
-def send_invite(sender, instance, created, **kwargs):
+def send_invite(sender, instance, created, **kwargs):  # pylint: disable=W0613
+    """
+    Send email when invite is created
+    """
     if instance.email is not None and created:
         instance.send()
 
 
 @receiver(post_save, sender=Comment, dispatch_uid="send_email_on_comment")
-def send_email_on_comment(sender, instance, created, **kwargs):
+def send_email_on_comment(sender, instance, created, **kwargs):  # pylint: disable=W0613
+    """
+    Send email when comment is created
+    """
     if created:
         instance.notify_assignees()
         instance.notify_mentioned_users()
 
 
 @receiver(pre_delete, sender=Board, dispatch_uid="delete_background_image")
-def delete_background_image(sender, instance, using, **kwargs):
+def delete_background_image(sender, instance, using, **kwargs):  # pylint: disable=W0613
+    """
+    Delete background image when board is deleted
+    """
     def _delete_file(path):
         """ Deletes file from filesystem. """
         if os.path.isfile(path):
@@ -53,8 +69,10 @@ def delete_background_image(sender, instance, using, **kwargs):
 
 
 @receiver(pre_delete, sender=CardFile, dispatch_uid="delete_card_file")
-def delete_card_file(sender, instance, using, **kwargs):
-
+def delete_card_file(sender, instance, using, **kwargs):  # pylint: disable=W0613
+    """
+    Delete card files when card is deleted
+    """
     def _delete_file(path):
         """ Deletes file from filesystem. """
         if os.path.isfile(path):
