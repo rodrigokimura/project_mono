c4119941bd277f45401c15fd52ca1de80e64c468
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2022-02-02 01:09:02 -0300

    feat(curriculum_builder): add new app
    BREAKING CHANGE:

---
+++ mono/curriculum_builder/apps.py
@@ -0,0 +1,7 @@
+"""Curriculum Builder's app config"""
+from django.apps import AppConfig
+
+
+class CurriculumBuilderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'curriculum_builder'
