b6ecbc368faa527754ed79ede9a343bd16f9d5be
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-05-24 17:13:40 -0300

    feat(checklists): add more sorting features

--- mono/checklists/views.py
+++ mono/checklists/views.py
@@ -1,11 +1,16 @@
-"""Todo Lists' views"""
+"""Checklists' views"""
+from __mono.permissions import IsCreator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from rest_framework import status
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import ChecklistMoveSerializer, TaskMoveSerializer
+from .models import Configuration
+from .serializers import (
+    ChecklistMoveSerializer, ConfigurationSerializer, TaskMoveSerializer,
+)
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
@@ -28,7 +33,7 @@ class ChecklistMoveApiView(LoginRequiredMixin, APIView):
         serializer = ChecklistMoveSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -44,5 +49,15 @@ class TaskMoveApiView(LoginRequiredMixin, APIView):
         serializer = TaskMoveSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ConfigAPIView(RetrieveUpdateAPIView):
+    """View to read or update user config"""
+
+    serializer_class = ConfigurationSerializer
+    permission_classes = [IsCreator]
+
+    def get_object(self):
+        return Configuration.objects.get_or_create(created_by=self.request.user)[0]
