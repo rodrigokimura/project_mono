c650769a0af082e0c934048c97281f1ece6592df
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-08-10 22:05:03 -0300

    feat(mono): add firebase cloud messaging backend

--- mono/__mono/apps.py
+++ mono/__mono/apps.py
@@ -1,6 +1,15 @@
 """Override admin configuration."""
 from django.contrib.admin.apps import AdminConfig
+from firebase_admin import credentials, initialize_app
 
 
 class MyAdminConfig(AdminConfig):
     default_site = '__mono.admin.MyAdminSite'
+
+    def ready(self) -> None:
+        from django.conf import settings
+        cred = credentials.Certificate(
+            str(settings.FIREBASE_AUTH_FILE)
+        )
+        initialize_app(cred)
+        return super().ready()
