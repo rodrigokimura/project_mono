ea51214dba17c49086d192a3d26e1055b3870571
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-09-05 18:37:30 -0300

    feat(mind_maps): remove endpoint restrictions

--- mono/mind_maps/urls.py
+++ mono/mind_maps/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path("mind-maps/", views.MindMapListView.as_view(), name="mind_map_list"),
     path("<uuid:pk>/", views.MindMapDetailView.as_view(), name="detail"),
     path("<uuid:pk>/sync/", views.FullSyncView.as_view(), name="sync"),
-    path("pg/", views.PlaygroundView.as_view(), name="playground"),
     # API urls
     path("api/", include(router.urls)),
 ]
