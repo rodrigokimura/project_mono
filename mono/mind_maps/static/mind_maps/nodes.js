a8e80bb28987a7d731403a6e5a1edcc0c6f281df
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-09-05 10:44:41 -0300

    feat(mind_maps): add feat to drag nodes

--- mono/mind_maps/static/mind_maps/nodes.js
+++ mono/mind_maps/static/mind_maps/nodes.js
@@ -6,9 +6,20 @@ class Node {
         this.position = [0, 0]
         this.size = [100, 30]
         this.editMode = false
+        this.connector = null
+        this.metadata = {}
     }
     get width() { return this.size[0] }
     get height() { return this.size[1] }
+    get children() {
+        return nodes.filter(node => node.parent === this)
+    }
+    get inputConnectors() {
+        return [this.connector]
+    }
+    get outputConnectors() {
+        return this.children.map(child => child.connector)
+    }
 
     static get(id) {
         for (let node of nodes) {
@@ -29,12 +40,14 @@ class Node {
             y >= (this.position[1] - this.height / 2),
         ].every(v => v === true)
     }
-    draw(reverseNext, reverseFirst) {
+    autoPosition(reverseNext, reverseFirst) {
         this.position = (new Positioner(this, reverseNext, reverseFirst)).find()
+    }
+    draw() {
         var x = this.position[0] - this.width / 2
         var y = this.position[1] - this.height / 2
         let nodeEl = $(`
-            <div id="${this.id}" class="node" style="display: inline; position: absolute; width: ${this.width}px; height: ${this.height}px; left: ${x}px; top: ${y}px;">
+            <div id="${this.id}" draggable="true" class="node" style="display: inline; position: absolute; width: ${this.width}px; height: ${this.height}px; left: ${x}px; top: ${y}px;">
                 <input type="text" tabindex="-1"  placeholder="<No name>" value="${this.name}" style="width: 100%; height: 100%;">
             </div>
         `)
@@ -50,8 +63,8 @@ class Node {
                 27: () => { this.leaveEditMode() },
             }
             let normalCommands = {
-                13: () => { this.createChild('', e.shiftKey).focus() },
-                9: () => { this.createSibling('', e.shiftKey).focus() },
+                13: () => { this.createChild('', e.shiftKey).enterEditMode().focus() },
+                9: () => { this.createSibling('', e.shiftKey).enterEditMode().focus() },
                 46: () => { if (e.shiftKey) this.delete(!e.ctrlKey) },
                 37: () => { this.selectNext('l') },
                 38: () => { this.selectNext('u') },
@@ -61,13 +74,12 @@ class Node {
                 113: () => { this.enterEditMode() },
             }
             if (this.editMode) {
-                if (!e.keyCode in editModeCommands) return
+                if (!(e.keyCode in editModeCommands)) return
                 editModeCommands[e.keyCode]()
                 return
             } else {
-                if (!e.keyCode in normalCommands) return
-                e.stopPropagation()
                 e.preventDefault()
+                if (!(e.keyCode in normalCommands)) return
                 normalCommands[e.keyCode]()
                 return
             }
@@ -86,17 +98,43 @@ class Node {
             this.unselect()
         })
         nodeEl.on('dragstart', e => {
-            console.log(e)
+            e.target.style.opacity = '0.4'
+            this.metadata = {
+                offsetX: e.offsetX,
+                offsetY: e.offsetY,
+            }
         })
+        nodeEl.on('dragend', e => {
+            e.target.style.opacity = '1'
+            let position = [
+                this.position[0] + e.offsetX - this.metadata.offsetX,
+                this.position[1] + e.offsetY - this.metadata.offsetY,
+            ]
+            this.metadata = null
+            this.move(position)
+        })
+    }
+    erase() {
+        $(`#${this.id}`).remove()
+        if (this.connector) this.connector.erase()
+    }
+    move(position) {
+        this.position = position
+        this.erase()
+        this.draw()
+        let connectors = this.inputConnectors.concat(this.outputConnectors)
+        for (let connector of connectors) {
+            connector.draw()
+        }
     }
     createNode(name, parent, reverseNext, reverseFirst) {
         let node = new Node(name, parent)
         nodes.push(node)
-        node.draw(reverseNext, reverseFirst)
+        node.autoPosition(reverseNext, reverseFirst)
+        node.draw()
         if (parent) {
             var connector = new Connector(node, parent)
             connector.draw()
-            node.connector = connector
         }
         toast(`Node ${node.name} created`)
         return node
@@ -113,6 +151,7 @@ class Node {
         $(`#${this.id}`).removeClass('selected')
         $(`#${this.id} input`).css('caret-color', 'black')
         setCaretToPos($(`#${this.id} input`)[0], $(`#${this.id} input`).val().length)
+        return this
     }
     leaveEditMode() {
         toast('Leaving edit mode')
@@ -120,6 +159,7 @@ class Node {
         $(`#${this.id}`).addClass('selected')
         $(`#${this.id} input`).css('caret-color', 'transparent')
         setCaretToPos($(`#${this.id} input`)[0], $(`#${this.id} input`).val().length)
+        return this
     }
     focus() {
         $(`#${this.id} input`).focus()
@@ -127,9 +167,14 @@ class Node {
     blur() {
         $(`#${this.id} input`).blur()
     }
+    printDetail() {
+        console.log(`Node ${this.name} at (${this.position[0]}, ${this.position[1]})`)
+        console.log(this)
+    }
     select() {
         $(`#${this.id}`).addClass('selected')
         this.focus()
+        this.printDetail()
     }
     unselect() {
         $(`#${this.id}`).removeClass('selected')
@@ -139,15 +184,13 @@ class Node {
             toast('Cannot delete root node')
             return
         }
-        $(`#${this.id}`).remove()
+        this.erase()
         nodes = nodes.filter(node => node.id !== this.id)
         toast(`Node ${this.name} deleted`)
         this.connector.delete()
         delete this.connector
         this.parent.focus()
-        if (cascade) {
-            this.deleteChildren(cascade)
-        }
+        if (cascade) this.deleteChildren(cascade)
     }
     deleteChildren(cascade = true) {
         for (let node of nodes) {
