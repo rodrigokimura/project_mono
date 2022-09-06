5659fe262a9cdb9b2f5b8a917d3346b67788802d
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2022-09-05 23:31:18 -0300

    feat(mind_maps): add support for touch inputs

--- mono/mind_maps/static/mind_maps/nodes.js
+++ mono/mind_maps/static/mind_maps/nodes.js
@@ -167,6 +167,29 @@ class Node {
             this.metadata = null
             this.move(position)
         })
+        nodeEl.on('touchstart', e => {
+            e.target.style.opacity = '0.4'
+            this.metadata = {
+                offsetX: e.targetTouches[0].screenX,
+                offsetY: e.targetTouches[0].screenY,
+            }
+        })
+        nodeEl.on('touchmove', e => {
+            e.preventDefault()
+            var touchLocation = e.targetTouches[0]
+            nodeEl[0].style.left = this.position[0] + touchLocation.screenX - this.metadata.offsetX - this.size[0] / 2 + 'px'
+            nodeEl[0].style.top = this.position[1] + touchLocation.screenY - this.metadata.offsetY - this.size[1] / 2 + 'px';
+        })
+        nodeEl.on('touchend', e => {
+            var t = e.currentTarget
+            e.target.style.opacity = '1'
+            let position = [
+                parseFloat(nodeEl[0].style.left.replace('px', '')),
+                parseFloat(nodeEl[0].style.top.replace('px', '')),
+            ]
+            this.metadata = null
+            this.move(position)
+        })
     }
     erase() {
         $(`#${this.id}`).remove()
