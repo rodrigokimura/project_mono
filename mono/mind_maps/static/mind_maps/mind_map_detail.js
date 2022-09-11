12764302d97b29bebd29631be0d4531b98654076
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2022-09-10 23:52:02 -0300

    feat(mind_maps): expand panel on zoom

--- mono/mind_maps/static/mind_maps/mind_map_detail.js
+++ mono/mind_maps/static/mind_maps/mind_map_detail.js
@@ -13,21 +13,10 @@ function loadMindMap() {
         url: `/mm/api/mind_maps/${MIND_MAP_ID}/`,
         onSuccess(r) {
             nodes = []
-            const MARGIN = 20
-            let maxX = Math.max(...r.nodes.map(n => n.position[0]))
-            let minX = Math.min(...r.nodes.map(n => n.position[0]))
-            let width = maxX - minX
-            let maxY = Math.max(...r.nodes.map(n => n.position[1]))
-            let minY = Math.min(...r.nodes.map(n => n.position[1]))
-            let height = maxY - minY
-            let expandX = (width < window.innerWidth / scale) ? (window.innerWidth / scale - width) / 2 : 0
-            let expandY = (height < window.innerHeight / scale) ? (window.innerHeight / scale - height) / 2 : 0
-            let dX = MARGIN - minX + expandX
-            let dY = MARGIN - minY + expandY
             r.nodes.forEach(n => {
                 let newNode = Node.getOrCreate(n.id)
                 newNode.name = n.name
-                newNode.position = [n.position[0] + dX, n.position[1] + dY]
+                newNode.position = n.position
                 newNode.fontSize = n.font_size
                 newNode.padding = n.padding
                 newNode.textStyle = {
@@ -40,13 +29,32 @@ function loadMindMap() {
                     newNode.parent = Node.getOrCreate(n.parent)
                 }
             })
-            initializePanel(
-                Math.max(window.innerWidth / scale, width) + 2 * MARGIN,
-                Math.max(window.innerHeight / scale, height) + 2 * MARGIN,
-            )
-            renderNodes()
+            reposition(nodes)
+            centralize()
         }
     })
+}
+
+function reposition(nodes) {
+    const MARGIN = 20
+    let maxX = Math.max(...nodes.map(n => n.position[0]))
+    let minX = Math.min(...nodes.map(n => n.position[0]))
+    let width = maxX - minX
+    let maxY = Math.max(...nodes.map(n => n.position[1]))
+    let minY = Math.min(...nodes.map(n => n.position[1]))
+    let height = maxY - minY
+    let expandX = (width < window.innerWidth / scale) ? (window.innerWidth / scale - width) / 2 : 0
+    let expandY = (height < window.innerHeight / scale) ? (window.innerHeight / scale - height) / 2 : 0
+    let dX = MARGIN - minX + expandX
+    let dY = MARGIN - minY + expandY
+    nodes.forEach(n => {
+        n.position = [n.position[0] + dX, n.position[1] + dY]
+    })
+    initializePanel(
+        Math.max(window.innerWidth / scale, width) + 2 * MARGIN,
+        Math.max(window.innerHeight / scale, height) + 2 * MARGIN,
+    )
+    renderNodes()
 }
 
 function renderNodes() {
@@ -55,7 +63,6 @@ function renderNodes() {
         n.draw()
         n.drawConnectors()
     })
-    centralize()
     storeState()
 }
 
