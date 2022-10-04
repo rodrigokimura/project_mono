9d9b6ee8d7f6ecceee2b8f88b8cfb5073412652e
rodrigokimura <rodrigoeitikimura@gmail.com>
2022-10-04 13:58:03 -0300

    feat(mind_maps): change connector to use svg

--- mono/mind_maps/static/mind_maps/connectors.js
+++ mono/mind_maps/static/mind_maps/connectors.js
@@ -1,4 +1,4 @@
-class Connector {
+class BaseConnector {
     constructor(node1, node2) {
         this.node1 = node1
         this.node2 = node2
@@ -6,6 +6,20 @@ class Connector {
     static get() {
         return $(`#${this.id} input`)[0], $(`#${this.id} input`)
     }
+    redraw() {
+        this.erase()
+        this.draw()
+    }
+    erase() {
+        this.el.remove()
+    }
+    delete() {
+        this.erase()
+    }
+    show() { this.el.show() }
+    hide() { this.el.hide() }
+}
+class DivLinearConnector extends BaseConnector {
     draw() {
         let x1 = this.node1.position[0]
         let y1 = this.node1.position[1]
@@ -23,16 +37,27 @@ class Connector {
         this.el.css('background-color', this.node1.colors.border)
         $(PANEL).append(this.el)
     }
-    redraw() {
-        this.erase()
-        this.draw()
-    }
-    erase() {
-        this.el.remove()
-    }
-    delete() {
-        this.erase()
-    }
-    show() { this.el.show() }
-    hide() { this.el.hide() }
 }
+
+class SvgLinearConnector extends BaseConnector {
+    draw() {
+        let x1 = this.node1.position[0]
+        let y1 = this.node1.position[1]
+        let x2 = this.node2.position[0]
+        let y2 = this.node2.position[1]
+
+        let width = Math.abs(x2 - x1) * scale
+        let height = Math.abs(y2 - y1) * scale
+
+        this.node1.connector = this
+        this.el = $(`
+            <svg class="connector" data-nodes="${this.node1.id}|${this.node2.id}" style="left: ${x2 * scale}px; top: ${y2 * scale}px; background-color: transparent;" width="${width}" height="${height}" overflow="visible" pointer-events="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <line x1="0" x2="${(x1 - x2) * scale}" y1="0" y2="${(y1 - y2) * scale}" stroke-width="${this.node1.borderSize * scale}"/>
+            </svg>
+        `)
+        this.el.css('stroke', this.node1.colors.border)
+        $(PANEL).append(this.el)
+    }
+}
+
+var Connector = SvgLinearConnector
